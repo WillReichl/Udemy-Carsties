38b61b084c245d0664bc2a731e354d96c04fa141
Will-Reichl <William.Reichl@dcsg.com>
2023-10-06 07:03:08 -0400

    Section 5 - WIP - End of Lesson 54

---
+++ src/IdentityService/Pages/Account/Register/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityService.Pages.Account.Register;
+
+[SecurityHeaders]
+[AllowAnonymous]
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
