15b89e972bb687f7e14017c21249991f54a27cc5
Will-Reichl <William.Reichl@dcsg.com>
2023-10-16 09:38:04 -0400

    End of Section 5

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,6 +1,7 @@
 using AuctionService.Consumers;
 using AuctionService.Data;
 using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -31,6 +32,15 @@ builder.Services.AddMassTransit(busRegConfig =>
     });
 });
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -44,9 +54,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-// We are running our app over HTTP.
-// app.UseHttpsRedirection();
+// We are running our app over HTTP and do not want: UseHttpsRedirection
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
