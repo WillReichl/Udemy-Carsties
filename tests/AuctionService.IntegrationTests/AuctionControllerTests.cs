7c50858017e8bb32eed38cfdd4e2ab0afe1e992a
Will-Reichl <William.Reichl@dcsg.com>
2024-05-28 07:50:10 -0400

    End of Appendix A: Testing
    - Add tests for bus using Mass Transit test harness
    - Add tests for auth using fake JWT bearer
    - Make use of Collection sharing to allow our integration tests to share a postgres container
    - Make project target NET 7.0 to match course and other projects

--- tests/AuctionService.IntegrationTests/AuctionControllerTests.cs
+++ tests/AuctionService.IntegrationTests/AuctionControllerTests.cs
@@ -6,7 +6,8 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace AuctionService.IntegrationTests;
 
-public class AuctionControllerTests : IClassFixture<CustomWebAppFactory>, IAsyncLifetime
+[Collection("Shared collection")]
+public class AuctionControllerTests : IAsyncLifetime
 {
     private readonly CustomWebAppFactory _factory;
     private readonly HttpClient _httpClient;
@@ -65,5 +66,111 @@ public class AuctionControllerTests : IClassFixture<CustomWebAppFactory>, IAsync
 
         // assert
         Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task CreateAuction_WithNoAuth_ShouldReturn401()
+    {
+        // arrange
+        var auction = new CreateAuctionDto { Make = "test" };
+
+        // act
+        var response = await _httpClient.PostAsJsonAsync($"api/auctions/", auction);
+
+        // assert
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task CreateAuction_WithAuth_ShouldReturn201()
+    {
+        // arrange
+        var auction = GetAuctionForCreate();
+        _httpClient.SetFakeJwtBearerToken(AuthHelper.GetBearerForUser("bob"));
+
+        // act
+        var response = await _httpClient.PostAsJsonAsync($"api/auctions/", auction);
+
+        // assert
+        response.EnsureSuccessStatusCode();
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        var createdAuction = await response.Content.ReadFromJsonAsync<AuctionDto>();
+        Assert.Equal("bob", createdAuction.Seller);
+    }
+
+    [Fact]
+    public async Task CreateAuction_WithInvalidCreateAuctionDto_ShouldReturn400()
+    {
+        // arrange
+        var auction = GetAuctionForCreate();
+        auction.Make = null;
+        _httpClient.SetFakeJwtBearerToken(AuthHelper.GetBearerForUser("bob"));
+
+        // act
+        var response = await _httpClient.PostAsJsonAsync($"api/auctions/", auction);
+
+        // assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task UpdateAuction_WithValidUpdateDtoAndUser_ShouldReturn200()
+    {
+        // arrange
+        var updateAuctionDto = GetAuctionForUpdate();
+        _httpClient.SetFakeJwtBearerToken(AuthHelper.GetBearerForUser("bob"));
+
+        // act
+        var response = await _httpClient.PutAsJsonAsync(
+            $"api/auctions/{FordGtId}",
+            updateAuctionDto
+        );
+
+        // assert
+        response.EnsureSuccessStatusCode();
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task UpdateAuction_WithValidUpdateDtoAndInvalidUser_ShouldReturn403()
+    {
+        // arrange
+        var updateAuctionDto = GetAuctionForUpdate();
+        _httpClient.SetFakeJwtBearerToken(AuthHelper.GetBearerForUser("not-bob"));
+
+        // act
+        var response = await _httpClient.PutAsJsonAsync(
+            $"api/auctions/{FordGtId}",
+            updateAuctionDto
+        );
+
+        // assert
+        Assert.Equal(HttpStatusCode.Forbidden, response.StatusCode);
+    }
+
+    private CreateAuctionDto GetAuctionForCreate()
+    {
+        return new CreateAuctionDto
+        {
+            Make = "testMake",
+            Model = "testModel",
+            ImageUrl = "testUrl",
+            Color = "testColor",
+            Mileage = 10,
+            Year = 2099,
+            ReservePrice = 100
+        };
+    }
+
+    private UpdateAuctionDto GetAuctionForUpdate()
+    {
+        return new UpdateAuctionDto
+        {
+            Make = "testMakeUpdate",
+            Model = "testModelUpdate",
+            Year = 2100,
+            Color = "Blue",
+            Mileage = 1000
+        };
     }
 }
